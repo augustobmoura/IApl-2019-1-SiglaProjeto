5470cefdbf09a3601991d0c0d1fe3306eeb55b4e
Murilo001 <murilo001os@gmail.com>
2019-04-29 19:56:24 -0300

    Modificações baseline 3

--- AS03/2. Sistema/src/main/java/br/uf/es/iapl/turmas/cli/AppCli.java
+++ AS03/2. Sistema/src/main/java/br/uf/es/iapl/turmas/cli/AppCli.java
@@ -1,53 +1,39 @@
 package br.uf.es.iapl.turmas.cli;
 
 import br.uf.es.iapl.turmas.dominio.Entidade;
-import br.uf.es.iapl.turmas.repositorio.Repositorio;
-import br.uf.es.iapl.turmas.serializador.fixedlentxt.SerializadorFactory;
-import br.uf.es.iapl.turmas.serializador.fixedlentxt.SerializadorFixedLength;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class AppCli {
 
+  @SuppressWarnings("unchecked")
   public static <T extends Entidade> void main(String[] args) throws IOException {
     final String log = System.getenv("LOG");
     if (log == null || log.isBlank()) {
       disableLogging();
     }
 
-    final EntityManagerFactory emFactory = Persistence
-        .createEntityManagerFactory("org.hibernate.tutorial.jpa");
+    final AppModule appModule = new AppModule(args);
 
-    final Repositorio repositorio =
-        new Repositorio(emFactory.createEntityManager());
-
-    final SerializadorFactory serializadorFactory = new SerializadorFactory(repositorio);
-
-    final OpcoesLinhaComando opcoesLinhaComando = new OpcoesLinhaComando(args);
-    final Optional<File> entrada = opcoesLinhaComando.getInput();
-
-    final Class<T> classe = (Class<T>) opcoesLinhaComando.getTabelaAlvo();
-    final SerializadorFixedLength<T> serializador =
-        serializadorFactory.encontrarFabrica(classe);
-
-    if (entrada.isEmpty()) {
+    if (appModule.getEntrada().isEmpty()) {
       final ImpressorEntidade<T> impressor = new ImpressorEntidade<>(
-          repositorio,
-          classe,
-          serializador
+          appModule.getRepositorio(),
+          appModule.getClasse(),
+          appModule.getSerializadorFactory().encontrarFabrica(appModule.getClasse())
       );
+
       impressor.imprimeTodosAlunos(System.out);
     } else {
-      final LeitorEntidade<T> leitorEntidade = new LeitorEntidade<>(repositorio, serializador);
-      leitorEntidade.gravarTodosDe(new FileReader(entrada.get()));
+      final LeitorEntidade<T> leitorEntidade = new LeitorEntidade<>(
+          appModule.getRepositorio(),
+          appModule.getSerializadorFactory().encontrarFabrica(appModule.getClasse())
+      );
+
+      leitorEntidade.gravarTodosDe(new FileReader(appModule.getEntrada().get()));
     }
   }
 
