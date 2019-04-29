adee831bbec99c6a22b0c3436f4efb631a98dbc9
Murilo001 <murilo001os@gmail.com>
2019-04-29 19:05:11 -0300

    Removido trabalho 3 por 2 estar incompleto

--- AS03/2. Sistema/src/main/java/br/uf/es/iapl/turmas/serializador/fixedlentxt/SerializadorFixedLength.java
+++
@@ -1,9 +0,0 @@
-package br.uf.es.iapl.turmas.serializador.fixedlentxt;
-
-public interface SerializadorFixedLength<T> {
-
-  T lerDeLinha(final String linha);
-
-  String converterParaLinha(final T entidade);
-
-}
